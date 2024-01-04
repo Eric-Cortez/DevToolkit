db70e1b91f61052e64d10b97f867f0e3c118e12d
Eric-Cortez <ecrcortez@gmail.com>
2024-01-03 21:18:19 -0800

    update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DevToolkit',
+  tagline: 'Navigating the Digital Frontier, Armed with Code',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
